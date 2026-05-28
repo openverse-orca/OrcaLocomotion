afc1e5348722779e656a2b9164a2dbb550cc6f3b
BenHuHuan <benhu1337@gmail.com>
2026-05-28 15:25:23 +0800

    Align vendored mjlab dependency pin

--- third_party/mjlab_rl/setup.py
+++ third_party/mjlab_rl/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 # Minimum dependencies required prior to installation
 INSTALL_REQUIRES = [
-    "mjlab==1.4.0",
+    "mjlab==1.2.0",
     "mujoco-warp==3.5.0",
 ]
 
